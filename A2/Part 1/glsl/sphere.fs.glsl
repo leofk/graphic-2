a280fc6a1b33be9f6b97ec3b9032e6c354795352
Leo Foord-Kelcey <73722423+leofk@users.noreply.github.com>
2022-01-30 04:41:46 +0200

    Delete A2 directory

--- A2/Part 1/glsl/sphere.fs.glsl
+++
@@ -1,5 +0,0 @@
-in vec3 interpolatedNormal;
-
-void main() {
-  	gl_FragColor = vec4(interpolatedNormal, 1.0);
-}
--- A2/README.md
+++
@@ -1,3 +0,0 @@
-# Assignment 2
-
-See [A2.pdf](A2.pdf) for the assignment specifications.
